60991f536f3964d5367b9411255c51283ec2782d
Michelle Snellen <msnellen0001@email.vccs.edu>
2026-03-16 08:57:49 -0400

    publish

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ const mobileNav = document.getElementById("mobile-nav");
 menuToggle.addEventListener("click", () => {
     mobileNav.classList.toggle("open");
 });
+
+let index = 0;
+const testimonials = document.querySelectorAll('.testimonial');
+
+function showNextTestimonial() {
+    testimonials[index].classList.remove('active');
+    index = (index + 1) % testimonials.length;
+    testimonials[index].classList.add('active');
+}
+
+setInterval(showNextTestimonial, 5000); // changes every 5 seconds
